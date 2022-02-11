93dc7d8e4c7264e041458a8c04398ab9eb5d5935
victor <victor.kim@polymtl.ca>
2022-02-11 16:12:37 -0500

    fix broadcast

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ io.on("connection",(socket)=>{
         data = parseObject(data);
         console.log(data);
         await messageService.createMessage(data.time,data.useremail,data.message);  
-        io.broadcast.emit("room1",data);  // send msg to all listener listening to room1 the right side json
+        socket.broadcast.emit("room1",data);  // send msg to all listener listening to room1 the right side json
     })
     
     socket.on("react",(data)=>{
