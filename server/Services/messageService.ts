a0307497bfd1dc3ed3c2f625500a3a339297cd59
victor <victor.kim@polymtl.ca>
2022-02-23 16:46:40 -0500

    ajout section socket dans le messageService

--- server/Services/messageService.ts
+++ server/Services/messageService.ts
@@ -1,13 +1,61 @@
 
+import { Socket,Server } from "socket.io";
 import MessageSchema from "../Entities/MessageSchema";
 import { Message } from "../Interface/Message";
 import databaseService from "./databaseService";
+import userService from "./userService";
 
 class MessageService {
+
     constructor() {
       this.getAllRoomMessages();
-    } 
+    }
+
     private messages:Array<Message>=[];
+    private io:Server;
+
+    /***************** socket section ****************/
+    initChat(server:Server) {
+      this.io=server;
+      this.connect();
+    }
+
+    connect():void {
+      this.io.on("connection",(socket:Socket)=>{
+        console.log(socket.id+" is connected");
+         this.sendMessage(socket);
+      });
+    }
+
+    sendMessage(socket:Socket) {
+      socket.on("msg",async (data)=> {    // listen for event named random with data
+        data = this.parseObject(data);
+        console.log(data);
+        await this.createMessage(data.time,data.useremail,data.message);  
+        socket.broadcast.emit("room1",data);  // send msg to all listener listening to room1 the right side json
+      })
+    }
+
+    disconnect(socket:Socket) {
+      socket.on("disconnect",()=>{
+        userService.getConnectedUsers().delete(socket.id);
+      });
+    }
+
+    parseObject(arg: any): Object {
+      if ((!!arg) && arg.constructor === Object) {
+          return arg
+      } else {
+          try {
+              return JSON.parse(arg);
+          } catch(E){
+              return {};
+          }
+      }
+    }
+
+
+    /************************** HTTP section *************************/
 
     getMessages():Array<Message> {
         return this.messages;
--- server/Services/socket.service.ts
+++ server/Services/socket.service.ts
@@ -1,7 +1,7 @@
 import http from 'http';
-import { Socket,Server } from "socket.io";
+import { Server } from "socket.io";
 import messageService from "./messageService";
-import userService from "./userService";
+
 
 class SocketService {
 
@@ -11,42 +11,9 @@ class SocketService {
       
   init(server:http.Server) {
      this.io=new Server(server);
-     this.connect();
+     messageService.initChat(this.io);
   }
 
-  connect():void {
-    this.io.on("connection",(socket:Socket)=>{
-      console.log(socket.id+" is connected");
-       this.sendMessage(socket);
-    });
-  }
-
-  parseObject(arg: any): Object {
-    if ((!!arg) && arg.constructor === Object) {
-        return arg
-    } else {
-        try {
-            return JSON.parse(arg);
-        } catch(E){
-            return {};
-        }
-    }
-}
-
-  sendMessage(socket:Socket) {
-    socket.on("msg",async (data)=> {    // listen for event named random with data
-      data = this.parseObject(data);
-      console.log(data);
-      await messageService.createMessage(data.time,data.useremail,data.message);  
-      socket.broadcast.emit("room1",data);  // send msg to all listener listening to room1 the right side json
-    })
-  }
-
-  disconnect(socket:Socket) {
-    socket.on("disconnect",()=>{
-      userService.getConnectedUsers().delete(socket.id);
-    });
-  }
 }
 
 const socketService=new SocketService();
