c0b04f31b2ee0b7ce68be4bf97c4d6d2b3e972bc
victor <victor.kim@polymtl.ca>
2022-02-23 16:00:23 -0500

    creation socket service

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
 import sampleRouter from './ping'
 import userData from './userData';
 import userController from './Controllers/userController';
-import messageService from './Services/messageService';
 import chatMessageController from './Controllers/chatMessageController';
-import userService from './Services/userService';
+import socketService from './Services/socket.service';
 
 const app = express();
 
@@ -29,54 +27,19 @@ app.use('/message',chatMessageController);
 
 
 const server = http.createServer(app); // server for http
-const io = new Server(server); // subclass server for socket
 
-let rooms = new Map<string, {name: string}>();
 
-io.on("connection",(socket)=>{
-    /*
+socketService.init(server);
+
+/*io.on("connection",(socket)=>{
+
     let user: any = socket.handshake.query.user;
     connectedUsers.set(socket.id,user);
-    */
-
-    socket.on("connection",(data)=>{
-        data=parseObject(data);
-        const mail:string=data.useremail as string;
-        console.log(mail);
-        userService.getConnectedUsers().set(mail,socket.id);
-        io.to(socket.id).emit("connected", `welcome user ` + `${mail}`);
-    })
-
-
-    function parseObject(arg: any): Object {
-        if ((!!arg) && arg.constructor === Object) {
-            return arg
-        } else {
-            try {
-                return JSON.parse(arg);
-            } catch(E){
-                return {};
-            }
-        }
-    }
-    
-     
-    socket.on("msg",async (data)=> {    // listen for event named random with data
-        data = parseObject(data);
-        console.log(data);
-        await messageService.createMessage(data.time,data.useremail,data.message);  
-        socket.broadcast.emit("room1",data);  // send msg to all listener listening to room1 the right side json
-    })
     
     socket.on("react",(data)=>{
         io.emit("room2",data);
     })
 
-    socket.on("disconnect",()=>{
-        userService.getConnectedUsers().delete(socket.id);
-    })
-
-    /*************test chat *******************/
     socket.on("CREATE_ROOM",({roomName})=>{
         console.log(`${roomName}`);
         rooms.set(socket.id, roomName);
@@ -84,9 +47,9 @@ io.on("connection",(socket)=>{
         io.to(roomName).emit("ROOM_CREATED", `welcome to room ` + `${roomName}`);
     });
 })
+*/
 
 server.listen(process.env.PORT || 8080, () => {
     console.log(`Server is running localhost:${app.get('PORT')}`);
 });
 
-export default io;
