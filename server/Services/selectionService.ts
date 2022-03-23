37fc83fbfc2652db1c65f26a194eae167d2bfdbd
Edwin Chiem <edwin.chiem@polymtl.ca>
2022-03-23 03:33:35 -0400

    local

---
+++ server/Services/selectionService.ts
@@ -0,0 +1,62 @@
+import { Server, Socket } from "socket.io";
+import roomService from "./roomService";
+//import { v4 as uuidv4 } from 'uuid';
+
+
+export class SelectionService {
+
+  constructor() { }
+
+  private io:Server;
+
+  initSelection(server:Server) {
+    this.io=server;
+    this.connect();
+  }
+
+  connect() {
+    this.io.on("connection",(socket:Socket)=>{
+        console.log("user start selection "+socket.id);
+        this.startSelect(socket);    
+        this.drawSelect(socket);
+        this.endSelect(socket);
+    })
+  }
+
+  startSelect(socket:Socket) {
+    socket.on("STARTSELECT",(data)=>{
+      console.log("data here " + data);
+      data=JSON.parse(data);
+      //data.id=uuidv4();
+      console.log("user "+socket.id+" starts selecting");
+      console.log("STARTSELECT");
+      console.log(data+""+roomService.getRoomNameBySocket(socket.id))
+      this.io.to(roomService.getRoomNameBySocket(socket.id) as string).emit("STARTSELECT",JSON.stringify(data));
+    })
+  }
+
+  drawSelect(socket:Socket) {
+    socket.on("DRAWSELECT",(data)=>{
+      data=JSON.parse(data);
+      this.io.to(roomService.getRoomNameBySocket(socket.id) as string).emit("DRAWSELECT",JSON.stringify(data));
+      //console.log("data x " + data.x);
+    })
+  }
+
+  endSelect(socket:Socket) {
+    socket.on("ENDSELECT",(data)=>{
+      data=JSON.parse(data);
+      console.log("user "+socket.id+" ends select");
+
+      console.log(data);
+      console.log("ENDLINE");
+      console.log(roomService.getRoomNameBySocket(socket.id)+" endline")
+      this.io.to(roomService.getRoomNameBySocket(socket.id) as string).emit("ENDLINE",{});
+    })
+  }
+
+
+}
+
+const selectionService=new SelectionService();
+export default selectionService;
