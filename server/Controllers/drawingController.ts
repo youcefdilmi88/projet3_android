43b567fd8880f43258d08eb383e53c6ebfd5e1bc
Steven <steven.diep@hotmail.ca>
2022-04-05 00:01:32 -0400

    console log

--- server/Controllers/drawingController.ts
+++ server/Controllers/drawingController.ts
@@ -178,6 +178,8 @@ const likeDrawing=async (req:Request,res:Response,next:NextFunction)=>{
     console.log("server drawname", drawingName);
     console.log("server useremail", useremail);
 
+    console.log("1", drawingService.drawings.get(drawingName)?.getLikes());
+    console.log("2", drawingService.drawings.get(drawingName)?.getLikes().indexOf(useremail));
     if(drawingService.drawings.get(drawingName)?.getLikes().indexOf(useremail)!=-1) {
        console.log("EMAIL EXIST");
         return res.status(404).json({message:HTTPMESSAGE.UALREADYLIKE});
