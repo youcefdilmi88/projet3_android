c3a2d8220b62cf5ef3775e20006292a4cc18f369
victor <victor.kim@polymtl.ca>
2022-04-05 00:56:53 -0400

    add on for remove like

--- server/Controllers/drawingController.ts
+++ server/Controllers/drawingController.ts
@@ -181,7 +181,7 @@ const likeDrawing=async (req:Request,res:Response,next:NextFunction)=>{
     console.log("1", drawingService.drawings.get(drawingName)?.getLikes());
     console.log("2", drawingService.drawings.get(drawingName)?.getLikes().indexOf(useremail));
     console.log("3", drawingService.drawings.get(drawingName));
-    
+
     if(drawingService.drawings.get(drawingName)?.getLikes().indexOf(useremail)!=-1) {
        console.log("EMAIL EXIST");
         return res.status(404).json({message:HTTPMESSAGE.UALREADYLIKE});
@@ -196,7 +196,7 @@ const likeDrawing=async (req:Request,res:Response,next:NextFunction)=>{
 }
 
 const removeLikeDrawing=(req:Request,res:Response,next:NextFunction)=>{
-    let drawingName:String=req.body.drawingName as String;
+    let drawingName:String=drawingService.addonDrawingName(req.body.drawingName) as String;
     let useremail:String=req.body.useremail as String;
 
     if(drawingService.drawings.get(drawingName)?.getLikes().indexOf(useremail)!=-1) {
